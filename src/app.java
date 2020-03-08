02a89269a41e3080a967a58045fd929d33e8d229
Gaurav <kishoregaurav@gmail.com>
2020-03-08 16:39:39 +1100

    webhook

--- src/app.java
+++ src/app.java
@@ -3,7 +3,7 @@ public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello AST:JIRA 1");
+		System.out.println("Hello AST:Test webhook");
 
 	}
 
